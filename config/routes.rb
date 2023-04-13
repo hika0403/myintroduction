7a829b164e03e87efd88990431d42e225e524e6a
hika0403 <egirlshikaru_0221@icloud.com>
2023-04-13 17:06:35 +0900

    メイン画像の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   get 'main/index'
   post 'main/index'
+  resources :main, only: [:index] do
+    resources :contacts, only: [:index] do
+      collection do
+        post 'contacts/confirm', to: 'contacts#confirm'
+        get 'done', to: 'contacts#done'
+      end
+    end
+  end
 end
