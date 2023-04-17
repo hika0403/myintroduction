4b05160a7b66877d105c28955be10ae764d57d1f
hika0403 <egirlshikaru_0221@icloud.com>
2023-04-17 10:22:16 +0900

    MailCatcherへ送信実装

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,24 +3,34 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
-  def confirm
+  def create
     @contact = Contact.new(contact_params)
     if @contact.valid?
-      render :action => 'confirm'
+      ContactMailer.send_mail(@contact).deliver_now
+      redirect_to ({action: :index}), notice: 'お問い合わせ内容を送信しました'
     else
-      render :action => 'index'
+      render :index
     end
   end
 
-  def done
-    @contact = Contact.new(contact_params)
-    if params[:back]
-      render :action => 'index'
-    else
-      ContactMailer.send_mail(@contact).deliver_now
-      render :action => 'done'
-    end
-  end
+  # def confirm
+  #   @contact = Contact.new(contact_params)
+  #   if @contact.valid?
+  #     render :action => 'confirm'
+  #   else
+  #     render :action => 'index'
+  #   end
+  # end
+
+  # def done
+  #   @contact = Contact.new(contact_params)
+  #   if params[:back]
+  #     render :action => 'index'
+  #   else
+  #     ContactMailer.send_mail(@contact).deliver_now
+  #     render :action => 'done'
+  #   end
+  # end
 
   private
     def contact_params
